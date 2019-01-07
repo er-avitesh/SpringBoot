1f50106765a3158e9a6caf1400882ac2be0b2d7d
Kesharwani <avitesh.kesharwani@capgemini.com>
2019-01-06 21:56:09 -0600

    Hello Controller

---
+++ src/main/java/learning/springboot/SpringBootRestServicesApplication.java
@@ -0,0 +1,19 @@
+package learning.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SpringBootRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServicesApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ src/main/java/learning/springboot/controller/HelloController.java
@@ -0,0 +1,12 @@
+package learning.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping(value = "/hello")
+	public String hello() {
+		return "Hello World";
+	}
+}
