54e4b87e2c3e5bc4c1c89be29c6f385dcb9c0763
Kesharwani <avitesh.kesharwani@capgemini.com>
2019-01-06 22:06:43 -0600

    Application Runner

--- src/main/java/learning/springboot/SpringBootRestServicesApplication.java
+++ src/main/java/learning/springboot/SpringBootRestServicesApplication.java
@@ -1,15 +1,21 @@
 package learning.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SpringBootRestServicesApplication {
+public class SpringBootRestServicesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestServicesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println("Hello world from Command Line Runner");
 	}
 
 	@Bean
